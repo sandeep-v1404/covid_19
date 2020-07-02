2abe2df3d59bbf3843fec894d5cbd3950b9e5922
Sandeep <valpasanisandeep@gmail.com>
2020-07-03 00:28:17 +0530

    Updates

---
+++ precache-manifest.4d4dbf5881e26004ecd08ea1644e7b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175a86ad92b102dda2ac091b2ae3b0e8",
+    "url": "/covid_19/index.html"
+  },
+  {
+    "revision": "8eaf2bed97c6ee7fae98",
+    "url": "/covid_19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8eaf2bed97c6ee7fae98",
+    "url": "/covid_19/static/js/2.e07a2316.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid_19/static/js/2.e07a2316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcb590df0f94bd78b41",
+    "url": "/covid_19/static/js/main.556e8f21.chunk.js"
+  },
+  {
+    "revision": "b5defa5aaa154683ca4d",
+    "url": "/covid_19/static/js/runtime-main.66960e00.js"
+  }
+]);
